1d8501bdc1fe25be1ae58b3ca6f50932d7d56832
nasib <mnnasib7777@gmail.com>
2022-07-09 00:22:13 +0300

    adding git repo

---
+++ assets/resources/js/general.js
@@ -0,0 +1,106 @@
+// todo should move this into seprate file only for search
+var data = [
+  {
+    id: 1,
+    text: " Hire professionals and agencies Service",
+    url: "/service",
+    icon: "fa-user-md",
+    heading: "Service",
+    searchText: null,
+  },
+  {
+    id: 2,
+    text: "Browse and buy Software",
+    url: "/software",
+    icon: "fa-check",
+    heading: "Software",
+    searchText: null,
+  },
+  {
+    id: 3,
+    text: " Apply to jobs posted by clients",
+    url: "/job",
+    icon: "fa-suitcase",
+    heading: "Jobs",
+    searchText: null,
+  },
+];
+(function($) {
+
+  $(".sub-nav").on('scroll', function() {
+    $val = $(this).scrollLeft();
+
+    if($(this).scrollLeft() + $(this).innerWidth()>=$(this)[0].scrollWidth){
+      $(".nav-next").hide();
+    } else {
+      $(".nav-next").show();
+    }
+
+    if($val == 0){
+      $(".nav-prev").hide();
+    } else {
+      $(".nav-prev").show();
+    }
+  });
+  console.log( 'init-scroll: ' + $(".nav-next").scrollLeft() );
+  $(".nav-next").on("click", function(){
+    $(".sub-nav").animate( { scrollLeft: '+=460' }, 200);
+
+  });
+  $(".nav-prev").on("click", function(){
+    $(".sub-nav").animate( { scrollLeft: '-=460' }, 200);
+  });
+})(jQuery);
+
+$(document).ready(function () {
+  $(".search-select-header").select2({
+    templateResult: formatState,
+    data,
+    placeholder: "Search",
+    matcher: matchStart,
+  });
+});
+
+$(".search-select-header").on("select2:select", function (e) {
+  var data = e.params.data;
+
+  window.location = `${data.url}?title=${data.searchText}`;
+});
+
+function matchStart(option, data) {
+  if (option.term) {
+    data.searchText = option.term;
+  } else {
+    data.searchText = data.heading;
+  }
+
+  return data;
+}
+
+function formatState(state) {
+  var $state = $(
+    `<span class="header-select-options">
+    <span class="search-content">
+    <i class="fa ${state.icon}" aria-hidden="true"></i>
+       <span class="heading"> ${state.searchText}</span>
+</span>
+    <span class="sp"> ${state.text}</span>
+    </span>`
+  );
+  return $state;
+}
+
+$(document).ready(function () {
+  setCatgoryNameIntoFilter();
+});
+function setCatgoryNameIntoFilter() {
+  var urlParams = new URLSearchParams(window.location.search);
+
+  if (urlParams.has("category_name")) {
+    $("#category_search_selected").text(urlParams.get("category_name"));
+  }
+}
+// $(".select2-custom").select2({
+//   placeholder:'Type Skills and Enter press',
+//   allowClear: true
+// });
