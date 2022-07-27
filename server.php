32941bba8ad71f663a8540d49b9611ed61281e93
duresameenrizwan <dure@getradgjof.is>
2022-07-27 23:26:18 +0500

    update server.php

--- server.php
+++ server.php
@@ -15,23 +15,23 @@ server {
 
    # pass the PHP scripts to FastCGI server listening on /var/run/php5-fpm.sock
    location ~ \.php$ {
-           try_files $uri /index.php =404;
-           fastcgi_pass unix:/var/run/php8-fpm.sock;
-           fastcgi_index index.php;
-           fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-           include fastcgi_params;
-   }
-    $uri = urldecode(
-        parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
-    );
+    root           /wwwroot/public/;
+    fastcgi_pass   20.50.64.3;
+    fastcgi_index  index.php;
+    fastcgi_param SCRIPT_FILENAME /wwwroot/public/$fastcgi_script_name;
+#   fastcgi_param  SCRIPT_FILENAME  $document_root$fastcgi_script_name;
+    include        fastcgi_params;
+}
+    //$uri = urldecode(
+    //    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
+    //);
     
     // This file allows us to emulate Apache's "mod_rewrite" functionality from the
     // built-in PHP web server. This provides a convenient way to test a Laravel
     // application without having installed a "real" web server software here.
-    if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
-        return false;
-    }
+    //if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
+//    return false;
+    //}
     
-    require_once __DIR__.'/public/index.php';
-     
+    //require_once __DIR__.'/public/index.php';
     }
