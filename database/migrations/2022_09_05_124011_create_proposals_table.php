1f9ec0daa87013e44d4bb1a26e2cf2db24c717c9
ArSalanAyoub <107831227+Arslan-Ayoub@users.noreply.github.com>
2022-09-05 19:38:37 +0500

    working on proposal tables

---
+++ database/migrations/2022_09_05_124011_create_proposals_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProposalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('proposals', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id')->index()->nullable();
+            $table->unsignedBigInteger('delivery_mode_id')->index()->nullable(); // delivery mode table needed
+            $table->integer('module_id')->index()->nullable();
+            $table->string('module_type')->nullable()->nullable();
+            $table->decimal('hourly_bid_rate', 28, 2)->default(0)->nullable();
+            $table->decimal('amount_receive', 28, 2)->default(0)->nullable();
+            $table->integer('start_hour_limit')->nullable();
+            $table->integer('end_hour_limit')->nullable();
+            $table->string('module_type')->nullable()->nullable();
+            $table->longText('cover_letter')->nullable()->nullable();
+            // create a table for dureforce fee
+            //create a table for milestones
+
+            $table->softDeletes();
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('proposals');
+    }
+}
