337b699df557073ce3c4c96af40f099f8e1f6b11
shahzaib341 <47967695+shahzaib341@users.noreply.github.com>
2022-09-19 01:02:24 +0500

    signup backend
    Working on signup backend functionality.

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\AdminNotification;
 use App\Providers\RouteServiceProvider;
+use App\Models\GeneralSetting;
 use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rules\Password;
 
 class RegisterController extends Controller
 {
@@ -39,6 +45,8 @@ class RegisterController extends Controller
     public function __construct()
     {
         $this->middleware('guest');
+        $this->middleware('regStatus')->except('registrationNotAllowed');
+        $this->activeTemplate = activeTemplate();
     }
 
     /**
@@ -47,14 +55,24 @@ class RegisterController extends Controller
      * @param  array  $data
      * @return \Illuminate\Contracts\Validation\Validator
      */
-    protected function validator(array $data)
+
+    public function showRegistrationForm()
     {
-        return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
-        ]);
+        $pageTitle = "Sign Up";
+        $info = json_decode(json_encode(getIpInfo()), true);
+        $mobile_code = @implode(',', $info['code']);
+        $countries = json_decode(file_get_contents(resource_path('views/partials/country.json')));
+        return view($this->activeTemplate . 'user.auth.register', compact('pageTitle', 'mobile_code', 'countries'));
     }
+
+    // protected function validator(array $data)
+    // {
+    //     return Validator::make($data, [
+    //         'name' => ['required', 'string', 'max:255'],
+    //         'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+    //         'password' => ['required', 'string', 'min:8', 'confirmed'],
+    //     ]);
+    // }
 
     /**
      * Create a new user instance after a valid registration.
@@ -62,12 +80,147 @@ class RegisterController extends Controller
      * @param  array  $data
      * @return \App\Models\User
      */
+    // protected function create(array $data)
+    // {
+    //     return User::create([
+    //         'name' => $data['name'],
+    //         'email' => $data['email'],
+    //         'password' => Hash::make($data['password']),
+    //     ]);
+    // }
+
+    protected function validator(array $data)
+    {
+
+        $general = GeneralSetting::first();
+        $password_validation = Password::min(6);
+        if ($general->secure_password) {
+            $password_validation = $password_validation->mixedCase()->numbers()->symbols()->uncompromised();
+        }
+        $agree = 'nullable';
+        if ($general->agree) {
+            $agree = 'required';
+        }
+
+        $validate = Validator::make($data, [
+            'firstname' => 'sometimes|required|string|max:50',
+            'lastname' => 'sometimes|required|string|max:50',
+            'email' => 'required|string|email|max:90|unique:users',
+            'password' => ['required', 'confirmed', $password_validation],
+            'username' => 'required|alpha_num|unique:users|min:6',
+            'captcha' => 'sometimes|required',
+            'type' => "required",
+            'agree' => $agree
+        ]);
+        return $validate;
+    }
+
+    public function register(Request $request)
+    {
+        return DB::transaction(function () use ($request) {
+            $this->validator($request->all())->validate();
+            if (isset($request->captcha)) {
+                if (!captchaVerify($request->captcha, $request->captcha_secret)) {
+                    $notify[] = ['error', "Invalid captcha"];
+                    return back()->withNotify($notify)->withInput();
+                }
+            }
+
+            Session::put('registerUsername', $request->get('username'));
+            event(new Registered($user = $this->create($request->all())));
+
+            $this->guard()->login($user);
+
+            return $this->registered($request, $user)
+                ?: redirect($this->redirectPath());
+        });
+
+    }
     protected function create(array $data)
     {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
-        ]);
+        $general = GeneralSetting::first();
+        //User Create
+        $user = new User();
+        $user->firstname = isset($data['firstname']) ? $data['firstname'] : null;
+        $user->lastname = isset($data['lastname']) ? $data['lastname'] : null;
+        $user->email = strtolower(trim($data['email']));
+        $user->password = Hash::make($data['password']);
+        $user->username = trim($data['username']);
+        $user->country_code = '';
+        $user->mobile = '';
+        $user->address = [
+            'address' => '',
+            'state' => '',
+            'zip' => '',
+            'country' => isset($data['country']) ? $data['country'] : null,
+            'city' => ''
+        ];
+        $user->status = 1;
+        $user->ev = $general->ev ? 0 : 1;
+        $user->sv = $general->sv ? 0 : 1;
+        $user->ts = 0;
+        $user->tv = 1;
+        $user->type = $data['type'];
+        $user->save();
+
+
+        $adminNotification = new AdminNotification();
+        $adminNotification->user_id = $user->id;
+        $adminNotification->title = 'New member registered';
+        $adminNotification->click_url = urlPath('admin.users.detail', $user->id);
+        $adminNotification->save();
+
+
+        //Login Log Create
+        $ip = $_SERVER["REMOTE_ADDR"];
+        $exist = UserLogin::where('user_ip', $ip)->first();
+        $userLogin = new UserLogin();
+
+        //Check exist or not
+        if ($exist) {
+            $userLogin->longitude = $exist->longitude;
+            $userLogin->latitude = $exist->latitude;
+            $userLogin->city = $exist->city;
+            $userLogin->country_code = $exist->country_code;
+            $userLogin->country = $exist->country;
+        } else {
+            $info = json_decode(json_encode(getIpInfo()), true);
+            $userLogin->longitude = @implode(',', $info['long']);
+            $userLogin->latitude = @implode(',', $info['lat']);
+            $userLogin->city = @implode(',', $info['city']);
+            $userLogin->country_code = @implode(',', $info['code']);
+            $userLogin->country = @implode(',', $info['country']);
+        }
+
+        $userAgent = osBrowser();
+        $userLogin->user_id = $user->id;
+        $userLogin->user_ip = $ip;
+
+        $userLogin->browser = @$userAgent['browser'];
+        $userLogin->os = @$userAgent['os_platform'];
+        $userLogin->save();
+
+
+        return $user;
+
+    }
+    public function checkUser(Request $request)
+    {
+        $exist['data'] = null;
+        $exist['type'] = null;
+        if ($request->email) {
+            $exist['data'] = User::where('email', $request->email)->first();
+            $exist['type'] = 'email';
+        }
+        if ($request->username) {
+            $exist['data'] = User::where('username', $request->username)->first();
+            $exist['type'] = 'username';
+        }
+        return response($exist);
+
+    }
+    public function registered()
+    {
+        return redirect()->route('user.verification.notice');
     }
 }
