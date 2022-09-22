2ab5d5bc8039e6da109b0fb3d7c2f60b572b7df0
ArSalanAyoub <107831227+Arslan-Ayoub@users.noreply.github.com>
2022-09-22 18:13:33 +0500

    added mail service provider

---
+++ app/Mail/SendSmtpMail.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class SendSmtpMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public $data;
+
+    public $template;
+    public function __construct($data,$template)
+    {
+
+        $this->data = $data;
+        $this->template = $template;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject($this->data['subject'])
+            ->view('register_email_template');
+    }
+}
