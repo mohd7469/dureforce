2e40d3604e1812dd11822614b4213c91c67897d5
ArSalanAyoub <107831227+Arslan-Ayoub@users.noreply.github.com>
2022-10-04 15:28:27 +0500

    changes to run pipeline

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,8 @@ Route::middleware('verified')->group(function () {
     Route::get('/get-cities', 'CommonProfileController@getCities')->name('get-cities');
     Route::get('/job-skills', 'SkillCategoryController@getSkills')->name('job.skills');
     Route::get('/job-profile-skills', 'SkillCategoryController@getSkills')->name('seller.profile');
-    
+
+
 });
 
 // --------------------------------------------------------------------------------------------------------------
