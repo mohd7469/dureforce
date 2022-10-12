021e5b85f4cd34901da6dc09e1f8115f93a0fd53
Mohsin <mohsin@larasoft.io>
2022-10-12 15:46:53 +0500

    set portfolio page

--- resources/views/templates/basic/portfolio/add_preview.blade.php
+++ resources/views/templates/basic/portfolio/add_preview.blade.php
@@ -1,104 +1,131 @@
-@extends($activeTemplate.'layouts.frontend')
-@section('content')
-<div class="container">
-
-    <div class="client_profile_con">    
-        <!----== Sidebar Container Start ==----->
-        <div class="sidebar-custom">
-            <ul class="sidebar-nav">
-                <li><a href="#">Add Project</a></li>
-                <li><a href="#">Add Details</a></li>
-                <li><a href="#">Preview</a></li>
-            </ul>
+    <div style="background: #F8FAFA ">
+        <div class="d-flex flex-wrap align-items-center justify-content-between">
+            <h3 class="card-title mt-2 ml-2">
+                 Previw
+            </h3>
+            
         </div>
+<section class="">
+    <div class="card-body pt-0 border-bottom" >
+        <div class="card-form-wrapper">
+            <div class="justify-content-center">
+                <div class="row">
+                    <h4 class="card-title mt-2 ml-2">
+                        Title
+                   </h4>
+                   <span>UX Design </span>
+                    
+                    <div class="col-xl-4 col-lg-4 col-md-4 form-group mt-4">
+                    
+                        <div class="card">
+                            <div class="img-wrapper-previw">
+                            <img src="http://127.0.0.1:8000/assets/images/software/61d885fd9368f1641580029.png" alt="image">
+                            </div>
+            
+                            <div class="card-body item-area item-card">
+                                <h4 class="">
+                                    <a href="javascript:void(0)">asd</a>
+                                </h4>
+                                <span>This is short project description</span>
+                     
 
-        <!----== Sidebar Container End ==----->
+            
+
+                            </div>
+                        </div>
+                
+                    </div>
+
+                    <div class="col-xl-4 col-lg-4 col-md-4 form-group mt-4">
+                    
+                        <div class="card">
+                            <div class="img-wrapper-previw">
+                            <img src="http://127.0.0.1:8000/assets/images/software/61d885fd9368f1641580029.png" alt="image">
+                            </div>
+            
+                            <div class="card-body item-area item-card">
+                                <h4 class="">
+                                    <a href="javascript:void(0)">asd</a>
+                                </h4>
+                                <span>This is short project description</span>
+                     
+
+            
+
+                            </div>
+                        </div>
+                
+                    </div>
+
+                </div>
+                
 
 
+                <div class="row pt-5 pb-5">
+                    
+                    <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-xs-12 form-group">
+                        
+                        <div class="col-md-7">
+                            <label>Suggested Skills</label>
+                            <ul class="skills-listing">
+                            <li>ui design</li>
+                            <li>research</li>
+                            </ul>
+                        </div>
+                    </div>
 
+                </div>
+                
 
-        </div>    
-        <!----== Client Info Section End ==----->
+                <div class="row">
 
+                    
+                    <div class="col-xl-8 col-lg-8 col-md-8 col-sm-12 col-xs-12 form-group">
+                        <label>Description</label>
+                        <p>Make sure you have approval from your clients to display the work you've done for them publicly.</p>
+                    </div>
 
+                </div>
+            </div>
+
+        </div>
     </div>
 
 
+    <div class="container pt-5 pb-5 pl=">
+        <div class="row">
+            <p class="pb-4">
+                September - 09
+            </p>
+        </div>
+        <div class="row">
 
-   
+                    
+            <div class="col-xl-8 col-lg-8 col-md-8 col-sm-12 col-xs-12 form-group">
+                <label>Project URL</label>
+                <p>Http/sdsdsds/dsdsa/dsads/sddss/sd/sads.com</p>
+            </div>
 
-@endsection
-@push('style')
+        </div>
+
+        <div id="outer" class="text-right">
+            <div class="inner">
+                <button type="button" class="pl-4   mt-20 w-70 cancel-job-btn">Back</button>
+            </div>
+            <div class="inner">
+                <button type="submit" class="pl-4 submit-btn mt-20 w-70 cretae-job-btn" id="submit-all">Publish</button>
+            </div>
+        </div>
+    </div>
+  
+  </section>
+  </div>
 <style>
-    .container{
-        max-width:1390px;
-        margin:0px auto;
-        display: block;
-    }
-    .client_profile_con{
-        width: 100%;
-        display: inline-block;
-    }
-    .sidebar-custom{
-        max-width: 230px; 
-        width:20%;
-        float: left;
-        display: inline-block;
-        height: 1059px;
-        background: #E6EEEE;
-        border-radius: 3px 0px 0px 3px;
-        padding: 28px 21px;
-    }
-    .client-info-section{
-        width: 80%;
-        float: left;
-        display: inline-block;
-        background: #F8FAFA;
-        border-radius: 0px 3px 3px 0px;
-    }
-    .sidebar-nav{
-        width: 100%;
-        display:inline-block;
-        padding: 0px;
-        margin: 0px;
-    }
-    .sidebar-nav li{
-        width: 100%;
-        display: inline-block;
-    }
-    .sidebar-nav li a {
-        font-weight: 600;
-        font-size: 14px;
-        line-height: 18px;
-        color: #007F7F;
-        width: 100%;
-        display: inline-block;
-        padding: 23px 10px 23px 50px;
-        text-decoration: none;
-        border-bottom: 1px solid #CBDFDF;
-        background-size: 31px;
-        display: inline-block;
-        background-position: center left !important;
-}
-ul.sidebar-nav li:nth-child(1) a {
-    background: url(/assets/images/job/user-icon.png) no-repeat;  
-}
-ul.sidebar-nav li:nth-child(2) a {
-    background: url(/assets/images/job/company-icon.png) no-repeat;  
-}
-ul.sidebar-nav li:nth-child(3) a {
-    background: url(/assets/images/job/dollars-icon.png) no-repeat;  
-}
-ul.sidebar-nav li:nth-child(4) a {
-    background: url(/assets/images/job/password-icon.png) no-repeat;  
+
+.card .img-wrapper-previw {
+    max-width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 </style>
-@endpush
-@push('script')
-    <script>
-        "use strict";
-        $(document).ready(function(){
-            $("#loginWithGmail").modal('show');
-        });
-    </script>
-@endpush
---
+++ resources/views/templates/basic/portfolio/side_nav.blade.php
@@ -0,0 +1,9 @@
+<div class="col-md-3 nopadding"> 
+    <div class="sidebar-custom">
+        <ul class="sidebar-nav nav nav-tabs" role="tablist">
+            <li role="tab" class="active"><a   data-toggle="tab" href="#addProject">Add Project <i class="fa-thin fa-octagon-check"></i></a></li>
+            <li role="tab" ><a data-toggle="tab" href="#addDetail">Add Details</a></li>
+            <li role="tab" ><a   data-toggle="tab" href="#addPreview">Preview</a></li>
+        </ul>
+    </div>
+</div>
