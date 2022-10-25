ce0b8067c713759d3651416807c2d9bf35eb6226
sajid444 <sajid.mehmood@clarisync.com>
2022-10-25 17:58:06 +0500

    install vue js added chat components

---
+++ resources/js/components/Chat/ChatContainerComponent.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Chat Container</div>
+
+                    <div class="card-body">
+                       
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+   import ChatHeader from "../components/Chat/ChatHeaderComponent.vue";
+   import ChatUsers from "../components/Chat/ChatUsersComponent.vue";
+   import Messages from "../components/Chat/MessageContainerComponent.vue";
+   import MessageInput from "../components/Chat/MessageInputComponent.vue";
+    
+    export default {
+        components: {
+       NavBar,
+       Spinner
+        }
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
