ba7d6d7f85c6519e07380253760371003d174f80
ArSalanAyoub <107831227+Arslan-Ayoub@users.noreply.github.com>
2022-11-09 22:01:16 +0500

    approved functionality added

--- resources/views/templates/basic/jobs/breadcrum.blade.php
+++ resources/views/templates/basic/jobs/breadcrum.blade.php
@@ -25,7 +25,7 @@
 </div> --}}
 <div class="breadcrumb" >
     <a  class="breadcrumb__step {{Route::is('buyer.job.single.view') ? 'breadcrumb__step--active':''}} ancortag-class" href="{{route('buyer.job.single.view',$job->uuid)}}">View Job Post</a>
-    @if($job->status->id == \App\Models\Job::$Pending)
+    @if($job->status->id == \App\Models\Job::$Pending || $job->status->id == \App\Models\Job::$Approved)
     <a class="breadcrumb__step  {{Route::is('buyer.job.invite.freelancer') ? 'breadcrumb__step--active':''}} ancortag-class" href="{{route('buyer.job.invite.freelancer',$job->uuid)}} ">Invite Freelancers</a>
     <a class="breadcrumb__step  {{Route::is('buyer.job.all.proposals') ? 'breadcrumb__step--active':''}} ancortag-class" href="{{route('buyer.job.all.proposals',$job->uuid)}}">Review Proposals ({{getNumberOfPropsals($job->uuid)}})</a>
 {{--    <a class="breadcrumb__step ancortag-class" style="padding-left:-15px !important;" href="#">Hire (0)</a>--}}
