11895421e51f3a5bf711af659e66efa6052f889c
shahzaib341 <47967695+shahzaib341@users.noreply.github.com>
2022-11-19 22:26:17 +0500

    * add on add project
    * add on add project

--- resources/views/templates/basic/portfolio/add_project.blade.php
+++ resources/views/templates/basic/portfolio/add_project.blade.php
@@ -5,7 +5,7 @@
           
                     <div class="col-md-6">
                         <p class="title-c">Add portfolio project</p>
-                        <label for="exampleInputEmail1">Project Title</label>
+                        <label for="exampleInputEmail1">Project Title *</label>
                         <input type="text" class="form-control" id="project_name" aria-describedby="emailHelp" placeholder="Enter a brief title of portfolio" name="name">
                     </div>
                 
@@ -14,7 +14,7 @@
                 <div class="row">
                 
                     <div class="col-md-6">
-                        <p class="rl-d">Related DF Job (optional) <br />
+                        <p class="rl-d">Related DF Job *<br />
                             Once you've completed contracts on DF, you'll be able to link your work.</p>
                     </div>
                     
