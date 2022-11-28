e3306dd51f611071837e0ce781ca8e1978805f6a
“mohsin” <mohsin@larasoft.io>
2022-11-28 22:52:57 +0500

    blade template design

--- resources/views/email-template/job/email_template.blade.php
+++ resources/views/email-template/job/email_template.blade.php
@@ -1,112 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>Email Template</title>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<style>
-    @import url('https://fonts.googleapis.com/css2?family=Mulish&display=swap');
-    @import url('https://fonts.googleapis.com/css2?family=Mulish:wght@800&display=swap');
-    
-*{ padding:0px; margin:0px;box-sizing: border-box;}
-img{ max-width: 100%;}
-
-@media only screen and (max-width:599px){
-.logo-container{
-    padding-left: 2% !important;
-}
-.body_container{
-    width: 100% !important;
-}
-ul.nav_listing li {
-    margin: 0px 0px 0px 0px !important;
-}
-ul.nav_listing li a {
-    font-size: 14px !important;
-    margin: 0px 8px !important;
-}
-.logo-m-container{
-    padding-left: 2%;
-}
-}
-@media only screen and (max-width:480px){
-    ul.nav_listing li a {
-    font-size: 11px !important;
-    margin: 0px 4px !important;
-}
-.nav_listing{
-    margin-top: 3px !important;
-    display: flex;
-     justify-content: center;
-     align-items: center;
-}
-h1.heading-s {
-    font-size: 20px !important;
-    line-height: 26px !important;
-}
-span.logo-con_mbl img {
-    width: 57% !important;
-    margin: 0px auto;
-    display: block;
-}
-.footer_cs1 {
-    text-align: center;
-    display: inline-block !important;
-}
-.footer_cs1 p {
-    width: 100% !important;
-}
-.tp-bg {
-    padding: 0px 10px 60px 10px !important;
-}
-
-}
-@media only screen and (max-width:375px){
- .d-cs {
-    display: inline-block !important;
-    text-align: center;
-}
-.nav_listing {
-    margin-top: -3px !important;
-    width: 100% !important;
-    /* display: inline-block !important; */
-    text-align: center !important;
-}
-.btns-cont a {
-    margin: 0px 0px 8px 0px !important;
-}
-}
-</style>
-</head>
-<body  style="padding: 0px; margin: 0px; box-sizing: border-box;">
-    <div style="width: 600px; margin: 0px auto; display: block; box-sizing: border-box;" class="body_container">
-    <header>
-        <div style=" display: inline-flex; width: 100%; margin-top: 20px; border-bottom: 1px solid #CBDFDF; padding-bottom: 15px; box-sizing: border-box;" class="d-cs">
-            <a href="#" style="width: 29%;" class="logo-m-container"><img src="/assets/images/emailtemplate/logo.svg" alt="Logo" class="logo-container;"></a>
-            <ul style=" float: right;  display: inline-flex; margin-top: 6px; width:69%;" class="nav_listing">
-                <li style=" margin-left: 8%;list-style: none; box-sizing: border-box;"><a href="#" style=" font-family: 'Mulish', sans-serif; font-weight: 500; font-size: 14px; line-height: 18px; color: #515151;
-                position: relative;">Jobs</a></li>
-                <li style=" margin-left: 8%;list-style: none; box-sizing: border-box;"><a href="#" style=" font-family: 'Mulish', sans-serif; font-weight: 500; font-size: 14px; line-height: 18px; color: #515151;
-                    position: relative;">Proposals</a></li>
-                <li style=" margin-left: 8%;list-style: none; box-sizing: border-box;"><a href="#" style=" font-family: 'Mulish', sans-serif; font-weight: 500; font-size: 14px; line-height: 18px; color: #515151;
-                    position: relative;">Services</a></li>
-                <li style=" margin-left: 8%;list-style: none; box-sizing: border-box;"><a href="#" style=" font-family: 'Mulish', sans-serif; font-weight: 500; font-size: 14px; line-height: 18px; color: #515151;
-                    position: relative;"> Softwares</a></li>
-                <li style=" margin-left: 8%;list-style: none; box-sizing: border-box;"><a href="#" style=" font-family: 'Mulish', sans-serif; font-weight: 500; font-size: 14px; line-height: 18px; color: #515151;
-                    position: relative;">Messages</a></li>
-            </ul>
-        </div>
-    </header>
-    <div style="box-sizing: border-box; background: rgba(239, 248, 248, 0.86) url(/assets/images/emailtemplate/bg-email.png) no-repeat;  border: 1px solid rgba(203, 223, 223, 0.32); border-radius: 8px; padding-bottom: 220px;
-    background-size: 80%; background-position: center 66%; margin: 24px 0px 24px 0px; position: relative; width: 100%;" class="tp-bg">
-          <span style=" width: 156px; margin: 20px auto 0px;  display: block;  position: relative; z-index: 2;" class="logo-con_mbl"><img src="/assets/images/emailtemplate//cong.png" alt="congrts"></span> 
-          <h1 style="width: 100%; font-family: 'Mulish', sans-serif; font-style: normal; font-weight: 800;  font-size: 28px;  line-height: 35px;
-          text-align: center;  color: #007F7F;  margin: 65px 0px 30px 0px; position: relative; z-index: 2;" class="heading-s">Congrats! You have been <br />
-            invited to submit a proposal!</h1>
-         <p style="box-sizing: border-box; font-weight: 600; font-size: 16px;  line-height: 20px;  text-align: center; color: #000000;  font-family: 'Mulish', sans-serif;
- position: relative;  z-index: 2;">Read more about the job below and <br /> submit a proposal if you are interested.</p>
-         
-    </div>
-
+@extends('layout_email.main')
+@section('content')
     <div style="box-sizing: border-box; background: #EFF8F8;  border-radius: 8px; padding: 24px 24px 8px 24px;  width: 100%;  display: inline-block;">
         <p style=" font-weight: 400;   font-size: 14px; line-height: 18px; color: #000000; width: 100%;
         display: inline-block;  font-family: 'Mulish', sans-serif; margin-bottom: 16px;">Hello!</p>
@@ -162,19 +55,82 @@ color: #000000; margin-bottom: 2px; width: 88%;">Thanks for your time,</p>
 <p style="box-sizing: border-box; font-family: 'Mulish', sans-serif;  font-size: 14px; line-height: 21px;
 color: #007F7F; margin-bottom: 27px; width: 88%; margin-top: 8px;">The DF Team</p>
 </div>
-<footer>
-    <div style="background: #58A7A7; width: 100%; display: inline-flex; padding: 20px 15px 16px 14px; margin-bottom: 0px; margin-top: 50px;" class="footer_cs1">
-        <span style="width: 25%;"><img src="/assets/images/emailtemplate/footer-logo.svg" alt="Footer logo"></span>
-       <p style="box-sizing: border-box; font-family: 'Mulish', sans-serif;  font-size: 13px; line-height: 21px;
-       color: #fff; margin-bottom: 13px; text-align: center; width: 50%;">Powered by getradgjof.is <br />
-        © 2022 Dure Force. All Rights Reserved. </p> 
-        <ul style="display: inline-flex;">
-            <li style="list-style:none;"><a href="#"><img src="/assets/images/emailtemplate/facebook-icon.svg"></a></li>
-            <li style="list-style:none; margin-left: 15px;"><a href="#"><img src="/assets/images/emailtemplate/linkedin-icon.svg"></a></li>
-            <li style="list-style:none;  margin-left: 15px;"><a href="#"><img src="/assets/images/emailtemplate/twitter-icon.svg"></a></li>
-        </ul>
-     </div>
-</footer>
-</div>
-</body>    
+
+@endsection
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Mulish&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Mulish:wght@800&display=swap');
+
+*{ padding:0px; margin:0px;box-sizing: border-box;}
+img{ max-width: 100%;}
+
+@media only screen and (max-width:599px){
+.logo-container{
+padding-left: 2% !important;
+}
+.body_container{
+width: 100% !important;
+}
+ul.nav_listing li {
+margin: 0px 0px 0px 0px !important;
+}
+ul.nav_listing li a {
+font-size: 14px !important;
+margin: 0px 8px !important;
+}
+.logo-m-container{
+padding-left: 2%;
+}
+}
+@media only screen and (max-width:480px){
+ul.nav_listing li a {
+font-size: 11px !important;
+margin: 0px 4px !important;
+}
+.nav_listing{
+margin-top: 3px !important;
+display: flex;
+ justify-content: center;
+ align-items: center;
+}
+h1.heading-s {
+font-size: 20px !important;
+line-height: 26px !important;
+}
+span.logo-con_mbl img {
+width: 57% !important;
+margin: 0px auto;
+display: block;
+}
+.footer_cs1 {
+text-align: center;
+display: inline-block !important;
+}
+.footer_cs1 p {
+width: 100% !important;
+}
+.tp-bg {
+padding: 0px 10px 60px 10px !important;
+}
+
+}
+@media only screen and (max-width:375px){
+.d-cs {
+display: inline-block !important;
+text-align: center;
+}
+.nav_listing {
+margin-top: -3px !important;
+width: 100% !important;
+/* display: inline-block !important; */
+text-align: center !important;
+}
+.btns-cont a {
+margin: 0px 0px 8px 0px !important;
+}
+}
+</style>
+
+  
 
