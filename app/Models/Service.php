ccecbe336f911a4e7585f92f7e37e68ffaa0394e
ArSalanAyoub <107831227+Arslan-Ayoub@users.noreply.github.com>
2022-12-08 14:02:10 +0500

    added softdelete for services

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -9,10 +9,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Service extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     const UPDATED_AT = null;
     const SERVICE_CREATION_IN_PROGRESS = 0;
