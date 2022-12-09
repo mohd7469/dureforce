b2f4e3a3215e462c584c4173f77e1e185fdb493e
ArSalanAyoub <107831227+Arslan-Ayoub@users.noreply.github.com>
2022-12-09 11:51:45 +0500

    added env for local dev qa staging production

--- database/migrations/software/2021_06_15_104532_create_software_table.php
+++ database/migrations/software/2021_06_15_104532_create_software_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateServicesTable extends Migration
+class CreateSoftwareTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,7 @@ class CreateServicesTable extends Migration
      */
     public function up()
     {
-        Schema::create('services', function (Blueprint $table) {
-
+        Schema::create('softwares', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->uuid('uuid')->index('uuid')->unique()->nullable();
             $table->unsignedBigInteger('user_id')->index()->nullable();
@@ -23,10 +22,12 @@ class CreateServicesTable extends Migration
             $table->unsignedBigInteger('status_id')->nullable();
             $table->string('title')->nullable();
             $table->longText('description')->nullable();
-            $table->decimal('rate_per_hour', 28,2)->default(0)->nullable();
-            $table->integer('estimated_delivery_time')->nullable();
+            $table->longText('software_application')->nullable();
+            $table->decimal('price', 28,2)->default(0)->nullable();
+            $table->integer('estimated_lead_time')->nullable();
             $table->longText('requirement_for_client')->nullable();
             $table->integer('number_of_simultaneous_projects')->nullable();
+
             $table->boolean('is_terms_accepted')->nullable()->default(false);
             $table->boolean('is_privacy_accepted')->nullable()->default(false);
             $table->boolean('is_active')->default(true)->nullable();
@@ -48,6 +49,6 @@ class CreateServicesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('services');
+        Schema::dropIfExists('software');
     }
 }
