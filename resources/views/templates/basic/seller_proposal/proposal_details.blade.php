1dded41e0ad5040ce006f3db345ce56b004dbb1e
“mohsin” <mohsin@larasoft.io>
2022-12-14 23:23:23 +0500

    seller dashboard figma design

---
+++ resources/views/templates/basic/seller_proposal/proposal_details.blade.php
@@ -0,0 +1,23 @@
+@extends($activeTemplate.'layouts.frontend')
+@section('content')
+
+proposal detaiils
+
+
+@endsection
+
+
+<style>
+</style>
+
+@push('script')
+
+<script>
+   'use strict';
+   $('#defaultSearch').on('change', function() {
+       this.form.submit();
+   });
+</script>
+
+
+@endpush
