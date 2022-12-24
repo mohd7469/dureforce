5790826a97c72be7982775dd653b7e62f7c8e9ce
“mohsin” <mohsin@larasoft.io>
2022-12-24 19:24:26 +0500

    bug fix

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/clear', function () {
     \Illuminate\Support\Facades\Artisan::call('optimize:clear');
 });
+// Route::get('cooning-soon', function () {
+//     return view('greeting');
+// });
 
 
 Route::get('/jobs-listing-old', [\App\Http\Controllers\Seller\JobController::class,'index'] )->name('jobs.listing.old');
