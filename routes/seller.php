51be901e2fa83d840e974d5f6a6becda430d6b3a
ArSalanAyoub <107831227+Arslan-Ayoub@users.noreply.github.com>
2022-12-27 13:02:23 +0500

    links added for seller proposals

--- routes/seller.php
+++ routes/seller.php
@@ -50,7 +50,7 @@ Route::name('seller.')->group(function () {
                     Route::get('/create-proposal/{uuid}',  [\App\Http\Controllers\Seller\ProposalController::class,'createProposal'] )->name('create');
                     Route::post('/validate-proposal',      [\App\Http\Controllers\Seller\ProposalController::class,'validatePropsal'] )->name('validate');
                     Route::post('proposal-store/{uuid}',   [\App\Http\Controllers\Seller\ProposalController::class,'savePropsal'])->name('store');
-                    Route::get('proposal-lists',           [\App\Http\Controllers\Seller\ProposalController::class,'index'])->name('index');
+                    Route::get('proposal-lists/{type?}',           [\App\Http\Controllers\Seller\ProposalController::class,'index'])->name('index');
                     Route::get('proposal-detail/{uuid}',           [\App\Http\Controllers\Seller\ProposalController::class,'details'])->name('detail');
 
                 });
