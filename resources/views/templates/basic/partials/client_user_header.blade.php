01c47dfe749ff2cf3e5d2cc0f8cda40d8b3795f3
shahzaib341 <47967695+shahzaib341@users.noreply.github.com>
2022-12-30 00:16:42 +0500

    Header issue resolved + dashboard changings
    Header issue resolved + dashboard changings

--- resources/views/templates/basic/partials/client_user_header.blade.php
+++ resources/views/templates/basic/partials/client_user_header.blade.php
@@ -146,7 +146,7 @@
                                         <button class="nav-button" type="button" data-bs-toggle="dropdown" data-display="static"
                                             aria-haspopup="true" aria-expanded="false">
                                             <a href="#"
-                                            @if (request()->routeIs('service')) class="active" @endif>@lang('Proposals')<span class="header-user-icon"><i class="las la-caret-down"></i></span></a>
+                                            @if (request()->routeIs('proposals')) class="active" @endif>@lang('Proposals')<span class="header-user-icon"><i class="las la-caret-down"></i></span></a>
                                         </button>
                                         <div class="dropdown-menu dropdown-center-menu dropdown-menu--sm p-0 border-0 dropdown-menu-right">
                                             <a href="{{url('coming-soon')}}"
@@ -170,7 +170,7 @@
                                     <div class="dropdown">
                                         <button class="nav-button" class="" type="button" data-bs-toggle="dropdown" data-display="static"
                                             aria-haspopup="true" aria-expanded="false">
-                                            <a href="{{ route('service') }}"
+                                            <a href="#"
                                             @if (request()->routeIs('service')) class="active" @endif>@lang('Service')<span class="header-user-icon"><i class="las la-caret-down"></i></span></a>
                                         </button>
                                         <div class="dropdown-menu dropdown-center-menu dropdown-menu--sm p-0 border-0 dropdown-menu-right">
@@ -191,7 +191,7 @@
                                     <div class="dropdown">
                                         <button class="nav-button" type="button" data-bs-toggle="dropdown" data-display="static"
                                             aria-haspopup="true" aria-expanded="false">
-                                            <a href="{{ route('software') }}"
+                                            <a href="#"
                                             @if (request()->routeIs('software')) class="active" @endif>@lang('Software')<span class="header-user-icon"><i class="las la-caret-down"></i></span></a>
                                         </button>
                                         <div class="dropdown-menu dropdown-center-menu dropdown-menu--sm p-0 border-0 dropdown-menu-right">
