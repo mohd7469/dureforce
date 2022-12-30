de125e61174a1ee56c8ed1732021f0a886cfbf62
Shakir Fattani <m.shakirfattani@gmail.com>
2022-12-30 16:59:15 +0400

    updated

--- test-image-upload.php
+++ test-image-upload.php
@@ -20,7 +20,7 @@ if (isset($_POST["submit"])) {
 <html>
 <body>
 
-<form action="index.php" method="post" enctype="multipart/form-data">
+<form action="test-image-upload.php" method="post" enctype="multipart/form-data">
     Select image to upload:
     <input type="file" name="fileToUpload" id="fileToUpload">
     <input type="submit" value="Upload Image" name="submit">
