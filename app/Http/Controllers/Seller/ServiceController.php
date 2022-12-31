4804f21b2c3cadc609ce873aa4fa259dec3ac541
sajid444 <sajid.mehmood@clarisync.com>
2022-12-31 22:28:00 +0500

    Fixed Issue

--- app/Http/Controllers/Seller/ServiceController.php
+++ app/Http/Controllers/Seller/ServiceController.php
@@ -47,14 +47,15 @@ class ServiceController extends Controller
     }
     public function show($uuid){
 
+        
         $pageTitle = "Service details";
         $service = Service::withAll()->where('uuid', $uuid)->firstOrFail();
         $service->views += 1;
         $service->save();
         $related_services = Service::withAll()->where('category_id', $service->category_id)->where('sub_category_id', $service->sub_category_id)->where('id','<>',$service->id)->where('status_id', Service::STATUSES['APPROVED'])->latest()->limit(4)->get();
         $selected_skills = $service->skills ? implode(',', $service->skills->pluck('id')->toArray()) : '';
-        
-        return view($this->activeTemplate . 'service_deatils', compact('pageTitle', 'service','selected_skills','related_services'));
+        $emptyMessage="No Data Found";
+        return view($this->activeTemplate . 'service_deatils', compact('pageTitle', 'service','selected_skills','related_services','emptyMessage'));
     
     }
     public function create($id = null)
