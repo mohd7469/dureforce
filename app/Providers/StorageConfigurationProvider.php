b8ffda6758e86f3eb7e7726c27c4a95faf1cb6c2
ArSalanAyoub <107831227+Arslan-Ayoub@users.noreply.github.com>
2023-03-17 21:09:03 +0500

    added dynamic storage credentials

---
+++ app/Providers/StorageConfigurationProvider.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Config;
+class StorageConfigurationProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $storage_credentials = getStorageCredentials();
+
+        if ($storage_credentials) {
+            config([
+                'filesystems.disks.azure' => [
+                    'driver'    => 'azure',
+                    'name'      => $storage_credentials->name,
+                    'key'       => $storage_credentials->password,
+                    'container' => env('AZURE_STORAGE_CONTAINER'),
+                    'url'       => $storage_credentials->prefix,
+                    'sas_url'   => $storage_credentials->host,
+                    'prefix'    => null,
+                ]
+            ]);
+            Log::info(["successfully assigned storage credentials"=>$storage_credentials]);
+        }
+    }
+}
