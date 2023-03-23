368ddb1c3e1cb9b7332b37aa5791b601174de959
sajid444 <sajid.mehmood@clarisync.com>
2023-03-23 20:40:31 +0500

    Added testimonials response portal and middleware and token expired  pages

---
+++ resources/views/layout_email/testimonial/testimonial.blade.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <style>
+        body {
+            margin: 0;
+            font-family: sans-serif;
+        }
+
+        .email-tag {
+            width: 100%;
+            text-align: center;
+            padding: 30px 0px;
+            background: #3690ff;
+            font-size: 20px;
+            color: white;
+            margin: 0;
+            font-weight: 600;
+        }
+
+        .img-logo {
+            text-align: center;
+        }
+
+        .name-tag {
+            text-align: center;
+            color: #585858;
+            margin-bottom: 5px;
+        }
+
+        .btm-line {
+            width: 71px;
+            border: 2px solid #3690ff;
+        }
+
+        .content-main {
+            width: 60%;
+            margin: auto;
+            color: #818080;
+            font-size: 17px;
+        }
+
+        b {
+            font-size: 18px;
+            font-weight: bolder;
+        }
+
+        .alert-msg {
+            align-content: center;
+            color: #ff0000e8;
+        }
+
+        .text-dark {
+            color: #000;
+        }
+
+    </style>
+</head>
+
+<body>
+    <div>
+        <p class="email-tag">This is a System Generated Email</p>
+        <br />
+        <div class="img-logo">
+            <img src="<?php echo  getImage(imagePath()['logoIcon']['path'] . '/logo.png') ?>" alt="">
+        </div>
+        <div class="content-main">
+            <h2 class="text-dark">Hello <?php echo $user_testimonial->client_name; ?></h2>
+            <p class="text-dark">
+                <?php echo $user_testimonial->message_to_client; ?>
+                 
+                <a href="<?php echo $response_url ?>">Click to Endorse <?php echo $user->full_name; ?></a>
+                You can help by sharing a brief testimonial for <?php echo $user->full_name; ?> profile
+            </p>
+            
+            <br />
+            <p class="text-dark">
+                Thank you,
+                <br>
+                The dureforce team
+            </p>
+
+            <br>
+        
+        </div>
+    </div>
+</body>
+
+</html>
