3c3bbff392491453b3c6328e152083454ee6421e
shahzaib341 <47967695+shahzaib341@users.noreply.github.com>
2023-03-23 23:40:50 +0500

    Offer listing and details pages set in freelancer and buyer
    Offer listing and details pages set in freelancer and buyer

---
+++ app/Http/Controllers/OffersController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ModuleOffer;
+use App\Models\Role;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class OffersController extends Controller
+{
+    public function  index(){
+        $user=Auth::user();
+        $last_role_id=getLastLoginRoleId();
+        if ( $last_role_id  == Role::$Freelancer ) {
+            $offers=ModuleOffer::withAll()->where('offer_send_to_id','=',$user->id)->where('is_active',1)->get();
+        }
+        else if( $last_role_id == Role::$Client ){
+            $offers=ModuleOffer::withAll()->where('offer_send_by_id','=',$user->id)->where('is_active',1)->get();
+        }
+
+        return view('templates.basic.offer.offer_listing',compact('offers','last_role_id'));
+    }
+}
