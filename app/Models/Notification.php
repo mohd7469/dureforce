70fefd7ea64803dcb6dd1030d67fe159b8624238
syed-ali-zar <104356809+syed-ali-zar@users.noreply.github.com>
2023-03-25 00:40:03 +0500

    Bugs and Notification Setup

---
+++ app/Models/Notification.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Notification extends Model
+{
+    use HasFactory;
+    protected $table='notifications';
+    protected $fillable = [
+
+        "uuid",
+        "user_id",
+        "title",
+        "body",
+        "payload",
+        "url",
+        "is_read",
+        "notification_type",
+        "deleted_at"
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
+
+
