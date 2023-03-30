ec0327371cd5a5bef28aeff3ab9d11a9d223a00d
sajid444 <sajid.mehmood@clarisync.com>
2023-03-30 12:49:16 +0500

    added status in user testimonial and resolved bugs

---
+++ database/migrations/production/2023_03_30_052659_add_status_id_in_user_testimonials_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddStatusIdInUserTestimonialsTable extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('user_testimonials', function (Blueprint $table) {
+            $table->unsignedBigInteger('status_id')->nullable()->after('id');
+            $table->unsignedBigInteger('created_by')->index('created_by')->nullable();
+            $table->unsignedBigInteger('updated_by')->index('updated_by')->nullable();
+            $table->unsignedBigInteger('deleted_by')->index('deleted_by')->nullable();
+            $table->foreign('status_id')->references('id')->on('statuses')->onDelete('RESTRICT')->onUpdate('RESTRICT');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('user_testimonials', function (Blueprint $table) {
+            $table->dropColumn(['status_id','created_by','updated_by','deleted_by']);
+        });
+    }
+}
