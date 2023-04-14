1ac2e5394745251a018f79a4d9b0644620cd8049
shahzaib341 <47967695+shahzaib341@users.noreply.github.com>
2023-04-15 00:25:13 +0500

    Minor changing on payment page
    Minor changing on payment page

--- routes/buyer.php
+++ routes/buyer.php
@@ -76,7 +76,7 @@ Route::name('buyer.')->prefix('buyer')->group(function () {
                 });
 
                 Route::post('job-offer-send', [\App\Http\Controllers\Buyer\OfferController::class,'sendOffer'])->name('offer.save');
-                Route::get('job-offer-sent/{id}', [\App\Http\Controllers\Buyer\OfferController::class,'offerSent'])->name('offer.sent');
+                Route::get('job-offer-sent/{id?}', [\App\Http\Controllers\Buyer\OfferController::class,'offerSent'])->name('offer.sent');
                 
                 Route::get('job-offer-send-response', [\App\Http\Controllers\Buyer\OfferController::class,'offerSuccessfullySubmitted'])->name('offer.success.alert');
 
