eb57d26f5c3c55b59da0c20cc32a7b8b534567ce
ArSal <107831227+Arslan-Ayoub@users.noreply.github.com>
2023-05-26 00:19:30 +0500

    modified contracts feedback table

--- database/migrations/production/2023_04_18_172935_create_not_recomended_reasons_table.php
+++ database/migrations/production/2023_04_18_172935_create_not_recomended_reasons_table.php
@@ -13,16 +13,16 @@ class CreateNotRecomendedReasonsTable extends Migration
      */
     public function up()
     {
-        Schema::create('not_recomended_reasons', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->unsignedBigInteger('role_id')->nullable();
-            $table->string('name')->nullable();
-            $table->string('slug')->nullable();
-            $table->boolean('is_active')->nullable();
-            $table->softDeletes();
-            $table->timestamps();
-            $table->foreign('role_id')->references('id')->on('roles');
-        });
+//        Schema::create('not_recomended_reasons', function (Blueprint $table) {
+//            $table->bigIncrements('id');
+//            $table->unsignedBigInteger('role_id')->nullable();
+//            $table->string('name')->nullable();
+//            $table->string('slug')->nullable();
+//            $table->boolean('is_active')->nullable();
+//            $table->softDeletes();
+//            $table->timestamps();
+//            $table->foreign('role_id')->references('id')->on('roles');
+//        });
     }
 
     /**
@@ -32,6 +32,6 @@ class CreateNotRecomendedReasonsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('not_recomended_reasons');
+//        Schema::dropIfExists('not_recomended_reasons');
     }
 }
