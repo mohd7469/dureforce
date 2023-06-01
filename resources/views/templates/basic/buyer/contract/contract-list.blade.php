c1c80ea14077ba04766f865b2ba135aac8bdeeb7
syed-ali-zar <104356809+syed-ali-zar@users.noreply.github.com>
2023-06-02 00:00:12 +0500

    Responsive Table on Both sides

--- resources/views/templates/basic/buyer/contract/contract-list.blade.php
+++ resources/views/templates/basic/buyer/contract/contract-list.blade.php
@@ -22,8 +22,8 @@
 
 
             <div class="tab-content">
-                <div class="listing_table_con card-body tab-pane active" id="all">
-                    <table>
+                <div class="listing_table_con card-body tab-pane active table-responsive" id="all">
+                    <table class="table">
                         <thead>
                         <th>Title</th>
                         <th>Status</th>
