2a41bff2274569fcc262604896a971eda8da1afa
ArSal <107831227+Arslan-Ayoub@users.noreply.github.com>
2023-06-06 23:13:58 +0500

    added table for blogs

--- database/migrations/production/2023_06_06_172353_create_blogs_table.php
+++ database/migrations/production/2023_06_06_172353_create_blogs_table.php
@@ -1,11 +1,13 @@
 <?php
 
+use App\Traits\DatabaseOperations;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 class CreateBlogsTable extends Migration
 {
+    use DatabaseOperations;
     /**
      * Run the migrations.
      *
@@ -15,10 +17,13 @@ class CreateBlogsTable extends Migration
     {
         Schema::create('blogs', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id')->index()->nullable();
             $table->string('title')->nullable();
-            $table->longText('description')->nullable();
+            $table->longText('description')->nillable();
             $table->boolean('is_active')->nullable();
+
             $table->boolean('is_featured')->nullable();
+            $this->addCommonDBFields($table);
             $table->softDeletes();
             $table->timestamps();
         });
