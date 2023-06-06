f42b8a3aea0b6a13a033db2505cc72224a62bb82
sajid444 <sajid.mehmood@clarisync.com>
2023-06-06 11:03:47 +0500

    fixed bugs

--- assets/resources/js/service/create-service.js
+++ assets/resources/js/service/create-service.js
@@ -26,6 +26,15 @@ $(document).ready(function () {
     var sub_category_id=$('#sub-category').val();
     fetchSkills(category_id,sub_category_id,false);
   }
+  $('.btn-link').click(function() {
+    var target = $($(this).data('target'));
+    
+    if (target.hasClass('show')) {
+      target.removeClass('show').addClass('collapsing');
+    } else {
+      target.removeClass('collapsing').addClass('collapse show');
+    }
+  });
   loadActiveTab();
   loadSelect2();
   baannerForm();
