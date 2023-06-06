95735b5cf115f039333a33891721227ea8dd7ada
ArSal <107831227+Arslan-Ayoub@users.noreply.github.com>
2023-06-06 22:37:28 +0500

    added blogs table

--- database/migrations/production/2023_06_06_172353_create_blogs_table.php
+++ database/migrations/production/2023_06_06_172353_create_blogs_table.php
@@ -16,7 +16,7 @@ class CreateBlogsTable extends Migration
         Schema::create('blogs', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('title')->nullable();
-            $table->longText('description')->nillable();
+            $table->longText('description')->nullable();
             $table->boolean('is_active')->nullable();
             $table->softDeletes();
             $table->timestamps();
